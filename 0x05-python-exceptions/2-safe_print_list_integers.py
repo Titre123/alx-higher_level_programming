8986a5b321699e276167158894aab1311290683a
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-06-21 04:16:47 +0100

    Update 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
-   for i in range(0,x):
-       string = ""
-       try:
-           if my_list[i].isdigit() == true:
-               string += my_list[i]
-            else:
-                pass
-            print(f'{string}\n')
-            last_char = string[-1]
-            length = string.index(last_char) + 1
-            return length
-        except:
+    count = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return 
