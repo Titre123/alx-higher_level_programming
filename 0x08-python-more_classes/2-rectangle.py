d7c9a572a18a67fe46d668bee8af3ba6ee74e07e
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-06-27 23:39:54 +0100

    Update 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -56,9 +56,17 @@ class Rectangle:
         self.__height = value
         
     def area(self):
-        '''Return th area of a Rectangle'''
+        """Calculates the area of a Rectangle instance
+        Returns:
+            Area of the the rectangle, given by height * width
+        """
         return self.__width * self.__height
-    
+
     def perimeter(self):
-        '''Return th perimeter of a Rectangle'''
-        return 2(self.__width + self.__height)
+        """Calculates the perimeter of a Rectangle instance
+        Returns:
+            Perimeter of the rectangle, given by 2 * (height + width)
+        """
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
