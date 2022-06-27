d6b0e4fc962be1a9a23a5d928d70bb450b3a665b
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-06-27 23:17:24 +0100

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         '''Initializes the data'''
 
-        self._width = width
-        self._height = height
+        self.width = width
+        self.height = height
 
         @property
         def width(self):
