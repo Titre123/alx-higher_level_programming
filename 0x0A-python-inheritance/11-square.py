6e63e7607a75d8f001a04e13e296c6f4c5d1621c
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-07-05 06:07:17 +0100

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module 10-square.
+"""Module 11-square.
 Creates a Square class.
 """
 
@@ -23,14 +23,16 @@ class Square(Rectangle):
         """
 
         self.integer_validator("size", size)
-        Rectangle.__init__(size, size)
+        super().__init__(size, size)
         self.__size = size
 
     def __str__(self):
-        return '[Square] {}/{}'.format(self.__size, self.__size)
+        return str("[Square] {}/{}".format(self.__size, self.__size))
 
     def area(self):
         """Computes the area of a Square instance.
         Overwrites the area() method from Rectangle.
         """
-        return self.__size * self.__size
+
+        return self.__size ** 2
+
