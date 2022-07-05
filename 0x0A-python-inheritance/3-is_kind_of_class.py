fcac6df1a7d7ebda10f12dc16c7e5b361fb517df
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-07-05 05:39:01 +0100

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+'''Module 3-is_kind_of_class.
+Finds if the object is an instance of, or if the object is an
+instance of a class that inherited from, the specified class.
+'''
+
+
+from sqlalchemy import Float
+
+
+def is_kind_of_class(obj, a_class):
+    '''
+    return True if obj is an instance of a_class or a superclass
+    and false if it is not
+
+    Args:
+        -obj
+        -a_class
+    '''
+    if isinstance(obj, a_class):
+        return True
+
+    else:
+        return False
