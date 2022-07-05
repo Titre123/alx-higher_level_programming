69b98e61ec0377aa316eba95eb84ec46e708d5a0
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-07-05 05:05:35 +0100

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+'''Module 0-lookup.py
+lookup the attributes and methods in a class
+'''
+
+
+def lookup(obj):
+    '''Returns that list of attributes and methods.
+    
+    Args:
+        - obj
+    '''
+
+    return dir(obj)
+  
