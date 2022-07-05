b1f5f8c5df96447f44dc81bd2c1716f7f2ffbd84
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-07-05 05:46:52 +0100

    Update 7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+'''Module 6-base_geometry.py
+Add integer_validator method to the basegrometry
+'''
+
+
+class BaseGeometry:
+    '''
+    Public instance method:
+        - def area(self)
+        - def integer_validator(self, name, value):
+    '''
+
+    def area(self):
+        '''
+        Public instance method
+            - raise exception
+        '''
+        raise Exception('Public instance method')
+
+    def integer_validator(self, name, value):
+        '''
+        Public instance method
+            - validates the value argument
+        '''
+        if type(value) == int:
+            raise TypeError('{} must be an integer'.format(name))
+
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
