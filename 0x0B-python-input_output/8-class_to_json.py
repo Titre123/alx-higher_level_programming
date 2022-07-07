8a19a3f25a92681632275463adff584c062ef515
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-07-07 02:50:49 +0100

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+"""Module 8-class_to_json.
+returns the dictionary description with simple data
+structure (list, dictionary, string, integer and boolean)
+for JSON serialization of an object
+"""
+
+
+def class_to_json(obj):
+    '''
+    returns the dictionary description with simple data
+    structure (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object
+
+    Args
+        - obj: object to be coverted to dictionary
+    '''
+    return obj.__dict__
