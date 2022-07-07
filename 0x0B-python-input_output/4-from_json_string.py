73d15d22d58798743fd68eed9ee37adac6f3d6ad
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-07-07 02:45:12 +0100

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Module 4-from_json_string.
+Returns an object (Python data structure) represented by a JSON string
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    '''
+    Returns an object (Python data structure) represented by a JSON string
+
+    Args
+        - my_str: object (string) represented in JSON
+    '''
+    return json.loads(my_str)
