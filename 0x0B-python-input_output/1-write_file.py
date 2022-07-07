062404084a53f10fe0457567eab7d41049e5797a
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-07-07 02:39:39 +0100

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Module 1-write_file.
+Write a string to a text file and return the number of characters
+"""
+
+
+def write_file(filename="", text=""):
+    '''
+    Write a string to a text file and return the number of characters
+
+    Args
+        - filename: name of the file
+        - text: string to write into file
+    '''
+    with open(file='filename', mode='w', encoding='utf-8') as file:
+        return file.write(text)
