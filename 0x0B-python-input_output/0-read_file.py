60d77c75876f8dc028b87e98806aeef01af8dfd8
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-07-07 02:38:24 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Module 0-read_file.
+Reads from filename and prints its contents to stdout.
+"""
+
+
+def read_file(filename=""):
+    '''
+    Reads from filename and prints
+    its contents to stdout.
+
+    Args
+        - filename: name of the file
+    '''
+    with open(file='filename', mode='r', encoding='utf-8') as file:
+        print(file.read(), end="")
