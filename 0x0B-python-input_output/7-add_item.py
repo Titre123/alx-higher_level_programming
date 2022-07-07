e4ac0dab40ef90df076def2b69c2f5a78b42a380
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-07-07 02:48:06 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""Module 7-add_item.
+Adds all arguments to a Python list, and then save them to a file
+"""
+
+import sys
+import os
+import json
+
+save_to_json_file = __import__('5-save_to_\
+    json_file.py').save_to_json_file
+
+load_from_json_file = __import__('6-load_from_\
+    json_file.py').load_from_json_file
+
+file = 'add_item.json'
+my_list = []
+
+if os.path.exists(file) and os.path.getsize > 0:
+    load_from_json_file(file)
+
+if len(sys.argv) > 1:
+    for args in sys.argv[1:]:
+        my_list.append(args)
+
+save_to_json_file(my_list, file)
