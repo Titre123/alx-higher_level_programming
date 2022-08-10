47abeb6ec06faef178edacdcb42b5e7287478486
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-08-10 04:10:45 +0100

    sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,5 @@
 -- updates the score of Bob to 10 in the table second_table
--- updates a value in a table
-UPDATE `second_table` SET `score` = 10 WHERE `name` = 'Bob';
+-- updates a value in
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = 'Bob';
