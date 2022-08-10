ce1aa8a0f94dc2e37dcd5b4b429b3ef74b89e304
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-08-10 03:33:42 +0100

    Update 5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
 -- prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server
 -- displays description of a table
-SHOW TABLES
+SHOW TABLES;
 DESCRIBE first_table;
 
