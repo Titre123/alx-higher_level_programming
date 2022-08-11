5fc33121dd0d938162be155fc2d80ceb388f8bbf
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-08-11 04:35:41 +0100

    sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- creates the table unique_id on your MySQL server
--- creates a table
+-- creates a table and make the id have a default 1 and unique
 CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
