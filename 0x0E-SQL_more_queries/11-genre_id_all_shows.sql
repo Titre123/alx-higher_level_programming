92f697c72ac139812fd8e6068511198e69914dd8
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-08-12 05:48:43 +0100

    sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,4 @@
 -- lists all shows contained in the database hbtn_0d_tvshows
 -- lists all shows contained in the database hbtn_0d_tvshows and select tv_shows.title - tv_show_genres.genre_id
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
