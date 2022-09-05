fe9f68169451fbdfc4bc25b88c20ac19dc8da93f
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-05 22:45:15 +0100

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,12 +2,12 @@
 const args = process.argv;
 
 function add (a, b) {
-    /* This is function takes two arguments and
-    return their sum
-    Args-
-        - a
-        - b
-    */
+  /* This is function takes two arguments and
+  return their sum
+  Args-
+    - a
+    - b
+  */
   console.log(Number(a) + Number(b));
 }
 
