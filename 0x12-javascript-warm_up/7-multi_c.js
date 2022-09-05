320e8406793ef89b2e8482868b4f7283d6ec46b8
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-05 22:30:37 +0100

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,6 +5,6 @@ if (!Number(args[2])) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < args[2]; i++) {
-    console.log(`C is fun`);
+    console.log('C is fun');
   }
 }
