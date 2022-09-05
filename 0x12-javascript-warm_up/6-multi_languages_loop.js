b1e8944824f4624e9e5617517d42d103a95760ef
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-05 22:22:00 +0100

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const string = ["C is fun","Python is cool","JavaScript is amazing"]
+const string = ['C is fun','Python is cool','JavaScript is amazing']
 
 for (let i = 0; i < string.length; i++){
     console.log(string[i])
