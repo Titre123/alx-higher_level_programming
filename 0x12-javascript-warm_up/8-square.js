2caba97e1a1d03d361d2cc05e67d73dc280638ac
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-05 22:39:02 +0100

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,11 +4,11 @@ const args = process.argv;
 if (!Number(args[2])) {
   console.log('Missing size');
 } else {
-    for (let i = 0; i < args[2]; i++) {
-      let Var = '';
-      for (let j = 0; j < args[2]; j++) {
-        Var += `X`;
-      }
-      console.log(Var);
+  for (let i = 0; i < args[2]; i++) {
+    let Var = '';
+    for (let j = 0; j < args[2]; j++) {
+      Var += `X`;
     }
+    console.log(Var);
+  }
 }
