a5f5da48c4922daf99718ee2c7eba1241ee283bf
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-05 22:17:13 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,8 +3,8 @@ const args = process.argv;
 
 if (args[2] === undefined) {
   console.log('No argument');
-}else if (args.length === 3) {
+} else if (args.length === 3) {
   console.log('Argument found');
-}else {
+} else {
   console.log('Arguments found');
 }
