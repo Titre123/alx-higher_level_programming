6dfbbcc05626e37ececdf931f3cd292ac20f1ef7
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-05 20:21:01 +0100

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 /* 0-javascript_is_amazing module */
 
-const c_program = 'C is fun';
+const cProgram = 'C is fun';
 const python = 'Python is cool';
 const javascript = 'JavaScript is amazing';
 
-console.log(c_program);
+console.log(cProgram);
 console.log(python);
 console.log(javascript);
