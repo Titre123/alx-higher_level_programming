4cb103c96cba95703b75d2b7fa75d9db3c3735b2
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-05 22:19:30 +0100

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const args = process.argv;
 
-if(!Number(args[2])){
-    console.log("Not a number")
-}
-else{
-    console.log(`My number: ${parseInt(args[2])}`)
+if (!Number(args[2])) {
+  console.log('Not a number')
+} else {
+  console.log(`My number: ${parseInt(args[2])}`)
 }
