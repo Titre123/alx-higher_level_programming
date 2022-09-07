2ca06dbfd622b7a32880e3b79670a7a8def18f87
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-07 04:16:37 +0100

    classes

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (((w = parseInt(w)) > 0) && ((h = parseInt(h)) > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let Var = '';
+      for (let j = 0; j < this.width; i++) {
+        Var += 'X';
+      }
+      console.log(Var);
+    }
+  }
+
+  rotate () {
+    const width = this.width;
+    const height = this.height;
+    this.width = height;
+    this.height = width;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+module.exports = Rectangle;
