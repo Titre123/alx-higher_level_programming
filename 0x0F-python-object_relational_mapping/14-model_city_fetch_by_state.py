d0372d604501c2ce447de67d2d9deca00dd471f9
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-11 20:37:20 +0100

    sql

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+class definition of a State and an instance
+ Base = declarative_base()
+"""
+
+
+if __name__ == '__main__':
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_city import City
+    from model_state import Base, State
+    import sys
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           ''.format(sys.argv[1], sys.argv[2],
+                                     sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for st, ct in session.query(
+            State, City).where(City.state_id
+                               == State.id).order_by(City.id).all():
+        print('{}: ({}) {}'.format(st.name, ct.id,
+                                 ct.name))
+    session.close()
