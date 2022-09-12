7600cb766734cd0e9b2904b6ab6290357ca106a3
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-12 05:54:44 +0100

    Update 11-model_state_insert.py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -10,15 +10,14 @@ if __name__ == '__main__':
     from model_state import Base, State
     import sys
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           ''.format(sys.argv[1], sys.argv[2],
-                                     sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_state = State(name="Louisiana")
-    session.add(new_state)
+    Louisiana = State(name='Louisiana')
+    session.add(Louisiana)
     session.commit()
-    print('{}'.format(new_state.id))
+    query = session.query(State).filter(State.name =='Louisiana').first()
+    print(f'{query.id}')
     session.close()
