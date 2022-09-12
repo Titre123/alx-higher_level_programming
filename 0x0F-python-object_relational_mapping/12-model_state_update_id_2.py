5ee83018ed322f24f27e4e198ceb30240b3e057b
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-12 05:56:37 +0100

    Update 12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,14 +10,14 @@ if __name__ == '__main__':
     from model_state import Base, State
     import sys
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           ''.format(sys.argv[1], sys.argv[2],
-                                     sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).where(State.id == 2).first()
-    state.name = 'New Mexico'
+    change = session.query(State).filter(
+        State.id == 2).first()
+    change.name = 'New Mexico'
     session.commit()
     session.close()
+    
