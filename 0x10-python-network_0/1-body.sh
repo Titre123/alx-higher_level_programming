188e6508d4a54ee76001bd54d521d5e53e92f7fd
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-23 00:40:46 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 #  Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-
-status = curl -i -X GET "$1" | grep 'HTTP/1.*' | awk '{print $2}'
-if [ "$status" -eq 200] ; then
-	curl -X GET "$1
-else
-	echo ""
-fi
+curl -i -X GET -L "$1"
