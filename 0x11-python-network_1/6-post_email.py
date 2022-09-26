6a8f4e0e15c24d8981cebdb9f76b632711f3e1ff
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-26 16:22:52 +0100

    Create 6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Python script that takes in a URL and an email,
+     sends a POST request to the passed URL with the
+     email as a parameter, and displays the body
+     of the response (decoded in utf-8)
+"""
+
+
+import requests
+import sys
+if __name__ == "__main__":
+    args = sys.argv
+    data = {'email': args[2]}
+    r = requests.post(args[1], data = data)
