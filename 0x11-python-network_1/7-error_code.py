b880a9cd1dd2540986059d92d7d9d2e1217abedb
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-09-26 16:23:53 +0100

    Create 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Python script that takes in a URL,
+     sends a request to the URL and displays
+      the body of the response (decoded in utf-8)
+"""
+
+
+import requests
+import sys
+if __name__ == "__main__":
+    status_code = None
+    args = sys.argv
+    try:
+        r = requests.get(args[1])
+        r.raise_for_status()
+        status_code = r.status_code
+    except requests.exceptions.RequestException as err:
+        print(f'Error code: {status_code}')
