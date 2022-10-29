d3d42840a4d564bcb22f8dd0674085e6f58d5056
Ola-Balogun Taiwo <99520815+Titre123@users.noreply.github.com>
2022-10-29 06:51:45 +0100

    Update 102-script.js

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,7 +1,9 @@
-$('INPUT#btn_translate').click( () => {
+$(document).ready(function() {
+  $('INPUT#btn_translate').click( () => {
    const value = $('INPUT#language_code').val();
    const url = 'https://stefanbohacek.com/hellosalut/?lang=' + value;
    $.get(url, function(data) {
      $('DIV#hello').html(data.hello);
    });
- })
+ });
+})
